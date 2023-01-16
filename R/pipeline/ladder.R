19d4c6bcf24534ae1bd595f401017d14699fecbf
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2023-01-16 11:09:32 +1100

    Update ladder.R

--- R/pipeline/ladder.R
+++ R/pipeline/ladder.R
@@ -214,13 +214,13 @@ pipeline_talents_data <- function(talents_resp) {
                                         class_talents = lapply(
                                             safely_reduce(loadout, "selected_class_talents"),
                                             function(talent) {
-                                                safely_reduce(talent, "spell_tooltip", "spell", "id")
+                                                safely_reduce(talent, "tooltip", "spell_tooltip", "spell", "id")
                                             }
                                         ),
                                         spec_talents = lapply(
                                             safely_reduce(loadout, "selected_spec_talents"),
                                             function(talent) {
-                                                safely_reduce(talent, "spell_tooltip", "spell", "id")
+                                                safely_reduce(talent, "tooltip", "spell_tooltip", "spell", "id")
                                             }
                                         )
                                     )

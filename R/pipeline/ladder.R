54f615759f629f611e31ba1be7c24f4c3a38405a
ElianHugh <elianhte@gmail.com>
2023-07-26 20:01:42 +1000

    Update ladder.R

--- R/pipeline/ladder.R
+++ R/pipeline/ladder.R
@@ -32,7 +32,7 @@ pipeline_leaderboard_data <- function(season, bracket, client) {
     resp <- safe_request(pvp_leaderboard_request(season, bracket, client))
     entries <- safely_reduce(resp, "entries")
     lapply(
-        entries[1:100],
+        entries[1:150],
         function(x) {
             char <- safely_reduce(x, "character")
             list(
